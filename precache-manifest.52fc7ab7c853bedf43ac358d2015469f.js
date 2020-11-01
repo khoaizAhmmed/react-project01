9a7c8f1adeee087f245a725b17a356457124089d
khoaiz Ahmmed Tusher <60465549+khoaizahmmed@users.noreply.github.com>
2020-11-01 15:17:25 +0600

    Add files via upload

---
+++ precache-manifest.52fc7ab7c853bedf43ac358d2015469f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c7235f686d693489a9064b2741eeed",
+    "url": "/react-project01/index.html"
+  },
+  {
+    "revision": "944901fe55f00702cfc9",
+    "url": "/react-project01/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "317d63933fc7e914bf27",
+    "url": "/react-project01/static/css/main.6db51c1c.chunk.css"
+  },
+  {
+    "revision": "944901fe55f00702cfc9",
+    "url": "/react-project01/static/js/2.08ebdff0.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-project01/static/js/2.08ebdff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317d63933fc7e914bf27",
+    "url": "/react-project01/static/js/main.44a67304.chunk.js"
+  },
+  {
+    "revision": "c2eaead2b7bc0b2c31ee",
+    "url": "/react-project01/static/js/runtime-main.0952aacc.js"
+  }
+]);
